a762628d9d6b6b5f17ca20673637faba4e2a802b
Mike Milner <mike.milner@priorilegal.com>
2016-02-22 16:09:50 -0500

    Give message#deliver a draft parameter

--- app/models/mailboxer/message.rb
+++ app/models/mailboxer/message.rb
@@ -22,27 +22,30 @@ class Mailboxer::Message < Mailboxer::Notification
 
   #Delivers a Message. USE NOT RECOMMENDED.
   #Use Mailboxer::Models::Messageable.send_message instead.
-  def deliver(reply = false, should_clean = true)
+  def deliver(reply = false, should_clean = true, draft = false)
     self.clean if should_clean
 
     #Receiver receipts
-    temp_receipts = recipients.map { |r| build_receipt(r, 'inbox') }
+    mailbox_type = draft ? "unsent" : "inbox"
+    temp_receipts = recipients.map { |r| build_receipt(r, mailbox_type) }
 
     #Sender receipt
-    sender_receipt = build_receipt(sender, 'sentbox', true)
+    mailbox_type = draft ? "drafts" : "sentbox"
+    sender_receipt = build_receipt(sender, mailbox_type, true)
 
     temp_receipts << sender_receipt
 
     if temp_receipts.all?(&:valid?)
-      Mailboxer::MailDispatcher.new(self, temp_receipts).call
+      Mailboxer::MailDispatcher.new(self, temp_receipts).call if !draft
       temp_receipts.each(&:save!)
 
       conversation.touch if reply
 
       self.recipients = nil
 
-      on_deliver_callback.call(self) if on_deliver_callback
+      on_deliver_callback.call(self) if on_deliver_callback && !draft
     end
+
     sender_receipt
   end
 end
