92d82b23133ff5f6372d1b34c1e60f67c57a8ab4
Mike Milner <mike.milner@priorilegal.com>
2016-03-09 16:39:46 -0500

    Encrypt after sanitize

--- app/models/mailboxer/message.rb
+++ app/models/mailboxer/message.rb
@@ -24,6 +24,7 @@ class Mailboxer::Message < Mailboxer::Notification
   #Use Mailboxer::Models::Messageable.send_message instead.
   def deliver(reply = false, should_clean = true, draft = false)
     self.clean if should_clean
+    self.body = [body].pack("u")
 
     #Receiver receipts
     mailbox_type = draft ? "unsent" : "inbox"
@@ -56,6 +57,7 @@ class Mailboxer::Message < Mailboxer::Notification
     self.attachment = attachment
 
     self.clean if should_clean
+    self.body = [body].pack("u")
 
     conversation.subject = self.subject if subject_changed?
 
